e71d97f3b4a91589846f796f6d34739bca40f493
Francesco Malvezzi <francesco.malvezzi@unimore.it>
2013-08-30 09:35:09 +0200

    Aggiunto rakefile

---
+++ rakefile.rb
@@ -0,0 +1,28 @@
+task :default => [:parse]
+
+task :loader do
+  sh %{ruby -I . file_loader.rb}
+end
+
+PARSERS = 8
+
+1.upto PARSERS do |i|
+  task "parser#{i}".to_sym do
+    sh %{ruby -I . parser_client.rb}
+  end
+end
+
+def parser_list
+  build_list = []
+  1.upto PARSERS do |i|
+    build_list << "parser#{i}".to_sym
+  end
+  build_list << :loader
+end
+
+@parser_list  = parser_list
+
+desc "parse a bulk file"
+multitask :parse => @parser_list do
+  puts "done parsing"
+end
